63a5350821da18c02b3801f04afdafb5d2da62e6
JulesDELTEIL <jules.delteil@epitech.eu>
2025-12-23 23:06:46 +0100

    feat(player): simple sprite animation and movement based animation

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,15 @@
 ** main.cpp
 */
 
-#include <events.hpp>
 #include <GameTool.hpp>
-
+#include <events.hpp>
 #include <ECS/DenseZipper.hpp>
-#include <interaction/components/player.hpp>
-#include <physic/components/velocity.hpp>
 #include <clock.hpp>
+
+#include <interaction/components/player.hpp>
+#include <display/components/animation.hpp>
+#include <physic/components/velocity.hpp>
+#include <sfml/components/sprite.hpp>
 
 #include "configs.hpp"
 #include "entities.hpp"
@@ -28,27 +30,59 @@ int main() {
         gtool.addMap(map);
 
     gtool.createComponent("window", eField::SYSTEM);
-    ECS::Entity last = gtool.createMap(MAP_BEGIN, 0);
-
+    gtool.createMap(MAP_BEGIN, 0);
     gtool.createEntity(PLAYER, "player", {100.f, 100.f});
-    gtool.sub<te::Keys>("key_input", [&gtool](te::Keys keys){
-        auto& players = gtool.getComponent<addon::intact::Player>();
-        auto& velocities = gtool.getComponent<addon::physic::Velocity2>();
 
-        for (auto &&[_, vel] : ECS::DenseZipper(players, velocities)) {
+    gtool.sub<te::Keys>("key_input", [&gtool](te::Keys keys){
+        auto& plays = gtool.getComponent<addon::intact::Player>();
+        auto& vels = gtool.getComponent<addon::physic::Velocity2>();
+        auto& anims = gtool.getComponent<addon::display::Animation>();
+        auto& sps = gtool.getComponent<addon::sfml::Sprite>();
+
+        for (auto &&[_, vel, anim, sp] :
+            ECS::DenseZipper(plays, vels, anims, sps)) {
+            bool xKeys = false;
+            bool yKeys = false;
             if (keys[te::Key::Z]) {
                 vel.y = -170.0;
+                yKeys = true;
             } else if (keys[te::Key::S]) {
                 vel.y = 170.0;
+                yKeys = true;
             } else {
                 vel.y = 0.0;
             }
             if (keys[te::Key::Q]) {
                 vel.x = -170.0;
+                xKeys = true;
+                auto scale = sp.sp.getScale();
+                if (scale.x > 0) {
+                    scale.x *= -1;
+                    sp.sp.setScale(scale);
+                }
             } else if (keys[te::Key::D]) {
                 vel.x = 170.0;
+                xKeys = true;
+                auto scale = sp.sp.getScale();
+                if (scale.x < 0) {
+                    scale.x *= -1;
+                    sp.sp.setScale(scale);
+                }
             } else {
                 vel.x = 0.0;
+            }
+            if (yKeys || xKeys) {
+                anim.curAnim = 1;
+                anim.timestamp.delay = anim.frameInfos[1].frameDELAY;
+                anim.timestamp.restart();
+            } else {
+                anim.curAnim = 0;
+                anim.timestamp.delay = anim.frameInfos[0].frameDELAY;
+                anim.timestamp.restart();
+            }
+            if (yKeys && xKeys) {
+                vel.x *= 0.75;
+                vel.y *= 0.75;
             }
         }
     });
